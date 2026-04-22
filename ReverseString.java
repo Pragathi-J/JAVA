cb01a734556fd9af458a706ee78f04c33039a679
Pragathi-J <82266060+Pragathi-J@users.noreply.github.com>
2026-04-22 20:09:02 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+class Main{
+  public static void main(String[] args){
+    String s = "reverse";
+    String rev = "";
+    for(int i=s.length()-1; i>=0;i--){
+        rev += s.charAt(i);
+    }
+    System.out.println(rev);
+  }
+}
