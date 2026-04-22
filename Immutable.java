64ca20eedea89dd6064738340020bbca904fd1cc
Pragathi-J <82266060+Pragathi-J@users.noreply.github.com>
2026-04-22 19:09:35 +0530

    Create Immutable.java

---
+++ Immutable.java
@@ -0,0 +1,16 @@
+final class Person{
+  private final String name;
+  private final double salary;
+  
+  public Person(){
+    this.name = name;
+  }
+  
+  public String getName(){
+    return name;
+  }
+  
+  public double getSalary(){
+    return salary;
+  }
+}
